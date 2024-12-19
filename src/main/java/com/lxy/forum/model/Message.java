d45374e852ee228f8b1ca50d6b376349a2743cc6
matexiang <matexy2024@126.com>
2024-12-19 16:36:49 +0800

    model

---
+++ src/main/java/com/lxy/forum/model/Message.java
@@ -0,0 +1,24 @@
+package com.lxy.forum.model;
+
+import lombok.Data;
+
+import java.util.Date;
+@Data
+public class Message {
+    private Long id;
+
+    private Long postUserId;
+
+    private Long receiveUserId;
+
+    private String content;
+
+    private Byte state;
+
+    private Byte deleteState;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+}
