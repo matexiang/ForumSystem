2e0847a4a341a67a9b36de096398a66eb7326a4c
matexiang <matexy2024@126.com>
2024-12-19 17:56:46 +0800

    .

--- src/main/java/com/lxy/forum/exception/GlobalExceptionHandler.java
+++ src/main/java/com/lxy/forum/exception/GlobalExceptionHandler.java
@@ -5,7 +5,6 @@ import com.lxy.forum.common.ResultCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
--- src/main/java/com/lxy/forum/model/User.java
+++ src/main/java/com/lxy/forum/model/User.java
@@ -1,26 +1,33 @@
 package com.lxy.forum.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.Date;
+@ApiModel("用户信息")
 @Data
 public class User {
     private Long id;
 
+    @ApiModelProperty("用户")
     private String username;
 
     private String password;
 
+    @ApiModelProperty("昵称")
     private String nickname;
 
+    @ApiModelProperty("电话号码")
     private String phoneNum;
-
+    @ApiModelProperty("邮箱")
     private String email;
 
     private Byte gender;
 
     private String salt;
 
+    @ApiModelProperty("头像地址")
     private String avatarUrl;
 
     private Integer articleCount;
