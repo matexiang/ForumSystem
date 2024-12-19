6b2c7042f1362810a8af07c6bc007237243db133
matexiang <matexy2024@126.com>
2024-12-19 16:32:31 +0800

    common

---
+++ src/main/java/com/lxy/forum/common/AppResult.java
@@ -0,0 +1,65 @@
+package com.lxy.forum.common;
+
+import lombok.Data;
+
+@Data
+public class AppResult<T> {
+
+    private int code;
+    private String message;
+    private T data;
+
+    public AppResult(int code, String message) {
+      this(code,message,null);
+    }
+
+
+    public AppResult(int code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static AppResult success(){
+        return new AppResult(ResultCode.SUCCESS.getCode(), ResultCode.SUCCESS.getMessage());
+
+    }
+
+    public static AppResult success(String message){
+        return new AppResult(ResultCode.SUCCESS.getCode(), message);
+
+    }
+
+    public static <T> AppResult<T> success(T data){
+        return new AppResult<>(ResultCode.SUCCESS.getCode(), ResultCode.SUCCESS.getMessage(),data);
+
+    }
+
+    public static <T> AppResult<T> success(String message,T data){
+        return new AppResult<>(ResultCode.SUCCESS.getCode(), message,data);
+
+    }
+
+    public static AppResult failed(){
+        return new AppResult(ResultCode.FAILED.getCode(), ResultCode.FAILED.getMessage());
+
+    }
+
+    public static AppResult failed(String message){
+        return new AppResult(ResultCode.FAILED.getCode(), message);
+
+    }
+
+    public static AppResult failed(ResultCode resultCode){
+        return new AppResult(ResultCode.FAILED.getCode(), ResultCode.FAILED.getMessage());
+
+    }
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/lxy/forum/common/ResultCode.java
@@ -0,0 +1,44 @@
+package com.lxy.forum.common;
+
+public enum ResultCode {
+
+
+    SUCCESS                        (0,"操作成功"),
+    FAILED                         (1000,"操作失败"),
+    FAILED_UNAUTHORIZED            (1001,"未授权"),
+    FAILED_PARAMS_VALIDATE        (1002,"参数校验失败"),
+    FAILED_FORBIDDEN               (1003,"禁⽌访问"),
+    FAILED_CREATE                  (1004,"新增失败"),
+    FAILED_NOT_EXISTS              (1005,"资源不存在"),
+    FAILED_USER_EXISTS             (1101,"⽤⼾已存在"),
+    FAILED_USER_NOT_EXISTS        (1102,"⽤⼾不存在"),
+    FAILED_LOGIN                 (1103,"⽤⼾名或密码错误"),
+    FAILED_USER_BANNED           (1104,"你已被禁⾔,请联系管理员,并重新登录"),
+    FAILED_TWO_PWD_NOT_S           (1105,"两次输⼊的密码不⼀致"),
+    ERROR_SERVICES                (2000,"服务器内部错误"),
+    ERROR_IS_NULL                  (2001,"IS NULL");
+
+
+    int code;
+
+    String  message;
+
+    ResultCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+
+        return "code: "+ code+ ";" + "message: " + message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
