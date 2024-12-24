f693bcc0468ea62ce5d599c9682419ef2c854aae
matexiang <matexy2024@126.com>
2024-12-24 13:01:04 +0800

    Create UserControllerTest.java

---
+++ src/test/java/com/lxy/forum/controller/UserControllerTest.java
@@ -0,0 +1,14 @@
+package com.lxy.forum.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserControllerTest {
+
+    @Test
+    void register() {
+    }
+}
