f476b12ab3ef0f13928b5d36497c49fd9cdda7b1
matexiang <matexy2024@126.com>
2024-12-24 13:00:53 +0800

    Create UUIDUtil.java

---
+++ src/main/java/com/lxy/forum/utils/UUIDUtil.java
@@ -0,0 +1,16 @@
+package com.lxy.forum.utils;
+
+import java.util.UUID;
+
+public class UUIDUtil {
+
+    public static String UUID36(){
+        return UUID.randomUUID().toString();
+    }
+
+    public static String UUID32(){
+        return UUID.randomUUID().toString().replace("-","");
+    }
+
+
+}
