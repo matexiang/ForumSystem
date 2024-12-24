5c553857d3f14468db94726861b9be1c3c21c4c6
matexiang <matexy2024@126.com>
2024-12-24 12:52:34 +0800

    Update ApplicationException.java

--- src/main/java/com/lxy/forum/exception/ApplicationException.java
+++ src/main/java/com/lxy/forum/exception/ApplicationException.java
@@ -9,6 +9,10 @@ public class ApplicationException extends RuntimeException{
     //在异常中持有一个错误信息
     protected AppResult errorResult;
 
+    public ApplicationException (AppResult errorResult) {
+        super(errorResult.getMessage());
+        this.errorResult = errorResult;
+    }
 
     public ApplicationException(String message) {
         super(message);
@@ -21,4 +25,9 @@ public class ApplicationException extends RuntimeException{
     public ApplicationException(Throwable cause) {
         super(cause);
     }
+
+    public AppResult getErrorResult() {
+        return errorResult;
+    }
+
 }
