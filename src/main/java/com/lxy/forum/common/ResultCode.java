60137dbde3f77e0bbe0eb1c811d31f4e5262d87f
matexiang <matexy2024@126.com>
2025-01-17 10:39:55 +0800

    Update ResultCode.java

--- src/main/java/com/lxy/forum/common/ResultCode.java
+++ src/main/java/com/lxy/forum/common/ResultCode.java
@@ -20,8 +20,9 @@ public enum ResultCode {
 
     FAILED_USER_ARTICLE_COUNT     (1201,"更新帖子数量失败"),
 
-    FAILED_BOARD_BANNED           (1201,"板块状态异常"),
-    FAILED_BOARD_ARTICLE_COUNT     (1201,"更新帖子数量失败");
+    FAILED_BOARD_BANNED           (1202,"板块状态异常"),
+    FAILED_BOARD_ARTICLE_COUNT     (1201,"更新帖子数量失败"),
+    FAILED_BOARD_NOT_EXISTS(1203,"板块不存在" );
 
 
 
