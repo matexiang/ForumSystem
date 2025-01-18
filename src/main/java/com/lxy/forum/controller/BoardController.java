3b58a3511ee9e1e13f3f78a0480e90dd5013de02
matexiang <matexy2024@126.com>
2025-01-18 09:10:21 +0800

    Update BoardController.java

--- src/main/java/com/lxy/forum/controller/BoardController.java
+++ src/main/java/com/lxy/forum/controller/BoardController.java
@@ -1,14 +1,19 @@
 package com.lxy.forum.controller;
 
 import com.lxy.forum.common.AppResult;
+import com.lxy.forum.common.ResultCode;
+import com.lxy.forum.exception.ApplicationException;
 import com.lxy.forum.model.Board;
 import com.lxy.forum.service.IBoardService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -39,4 +44,25 @@ public class BoardController {
         }
         return AppResult.success(boards);
     }
+
+    @ApiOperation("获取板块信息")
+    @RequestMapping("getById")
+    public AppResult<Board> getById(@ApiParam("板块Id") @RequestParam("id") @NonNull Long id){
+        //调用Service
+        Board board = boardService.selectById(id);
+
+        //对查询的结果进行检验
+        if(board == null || board.getDeleteState() == 1){
+            //打印日志
+            log.warn(ResultCode.FAILED_BOARD_NOT_EXISTS.toString());
+            //抛异常
+            throw new ApplicationException(AppResult.failed(ResultCode.FAILED_BOARD_NOT_EXISTS));
+
+        }
+
+        //返回结果
+        return AppResult.success(board);
+    }
+
+
 }
