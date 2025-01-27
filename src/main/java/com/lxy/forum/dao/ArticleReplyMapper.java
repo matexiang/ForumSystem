5077dda6770324275f45c13d338620f3ca28a182
matexiang <matexy2024@126.com>
2025-01-27 18:20:47 +0800

    Update ArticleReplyMapper.java

--- src/main/java/com/lxy/forum/dao/ArticleReplyMapper.java
+++ src/main/java/com/lxy/forum/dao/ArticleReplyMapper.java
@@ -1,7 +1,11 @@
 package com.lxy.forum.dao;
 
+import com.lxy.forum.model.Article;
 import com.lxy.forum.model.ArticleReply;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface ArticleReplyMapper {
@@ -14,4 +18,11 @@ public interface ArticleReplyMapper {
     int updateByPrimaryKeySelective(ArticleReply row);
 
     int updateByPrimaryKey(ArticleReply row);
+
+
+    /*
+    * 根据帖子id查询所有回复
+    *articleId
+    *  */
+    List<ArticleReply> selectByArticleId (@Param("articleId") Long articleId);
 }
