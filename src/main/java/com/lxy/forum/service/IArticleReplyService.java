5c5ca9d8827f8f6f02d1bfbc84aa4aeb421352e8
matexiang <matexy2024@126.com>
2025-01-27 18:13:44 +0800

    Update IArticleReplyService.java

--- src/main/java/com/lxy/forum/service/IArticleReplyService.java
+++ src/main/java/com/lxy/forum/service/IArticleReplyService.java
@@ -1,12 +1,22 @@
 package com.lxy.forum.service;
 
 import com.lxy.forum.model.ArticleReply;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface IArticleReplyService {
 
     @Transactional
     void create (ArticleReply articleReply);
+
+    /*
+     * 根据帖子id查询所有回复
+     *articleId
+     *  */
+    List<ArticleReply> selectByArticleId ( Long articleId);
+
 
 
 }
