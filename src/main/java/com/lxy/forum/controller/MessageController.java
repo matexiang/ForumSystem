ff64b38b3dd8568838b1525297d3d75153394b93
matexiang <matexy2024@126.com>
2025-02-01 17:40:45 +0800

    Update MessageController.java

--- src/main/java/com/lxy/forum/controller/MessageController.java
+++ src/main/java/com/lxy/forum/controller/MessageController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Slf4j
 @Api(tags = "站内信接口")
@@ -75,5 +76,84 @@ public class MessageController {
         return AppResult.success(count);
 
     }
+
+    @ApiOperation("查询用户的所有站内信")
+    @GetMapping("/getAll")
+    public AppResult<List<Message>> getAll(HttpServletRequest request){
+
+        //获取当前登录的用户
+        HttpSession session = request.getSession(false);
+        User user = (User) session.getAttribute(AppConfig.USER_SESSION);
+        //调用service
+        List<Message> messages = messageService.selectByReceiveUserId(user.getId());
+
+        return  AppResult.success(messages);
+
+
+    }
+
+    @ApiOperation("更新为已读")
+    @PostMapping("/markRead")
+    public AppResult markRead(HttpServletRequest request,
+                              @ApiParam("站内信id") @RequestParam("id") @NonNull Long id){
+        //根据id查询站内信
+        Message message = messageService.selectById(id);
+        if (message == null || message.getDeleteState() == 1) {
+
+            return AppResult.failed(ResultCode.FAILED_NOT_EXISTS);
+
+        }
+        //校验站内信是不是自己的
+        HttpSession session = request.getSession(false);
+        User user = (User) session.getAttribute(AppConfig.USER_SESSION);
+        if (user.getId() != message.getReceiveUserId()) {
+            return AppResult.failed(ResultCode.FAILED_FORBIDDEN);
+        }
+        //调用service,把状态更新成已读
+        messageService.updateStateById(id,(byte)1);
+
+        return AppResult.success();
+
+    }
+
+    /**
+     * 回复站内信
+     * @param request
+     * @param repliedId
+     * @param content
+     * @return
+     */
+    @ApiOperation("回复站内信")
+    @PostMapping("/reply")
+    public AppResult reply (HttpServletRequest request,
+                            @ApiParam("要回复的站内信id") @RequestParam("repliedId") @NonNull  Long repliedId,
+                            @ApiParam("站内信内容") @RequestParam("content") @NonNull String content){
+
+        //校验当前用户状态
+        HttpSession session = request.getSession(false);
+        User user = (User) session.getAttribute(AppConfig.USER_SESSION);
+        if (user.getState() == 1) {
+            return AppResult.failed(ResultCode.FAILED_USER_BANNED);
+        }
+        //校验要回复对的站内信状态
+        Message existsMessage = messageService.selectById(repliedId);
+        if(existsMessage == null || existsMessage.getDeleteState() == 1){
+            return AppResult.failed(ResultCode.FAILED_ARTICLE_NOT_EXISTS);
+        }
+        if(user.getId() == existsMessage.getPostUserId()){
+            return AppResult.failed("不能给自己站内信回复");
+        }
+        //构造对象
+        Message message = new Message();
+        message.setPostUserId(user.getId());
+        message.setReceiveUserId(existsMessage.getReceiveUserId());
+        message.setContent(content);
+
+        messageService.reply(repliedId,message);
+
+        return AppResult.success();
+    }
+
+
 
 }
