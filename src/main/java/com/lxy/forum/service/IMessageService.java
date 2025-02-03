291b2ba42000cff4c05435a70e93d5adf02e40cc
matexiang <matexy2024@126.com>
2025-02-03 14:06:17 +0800

    Update IMessageService.java

--- src/main/java/com/lxy/forum/service/IMessageService.java
+++ src/main/java/com/lxy/forum/service/IMessageService.java
@@ -2,6 +2,10 @@ package com.lxy.forum.service;
 
 
 import com.lxy.forum.model.Message;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 public interface IMessageService {
@@ -11,4 +15,45 @@ public interface IMessageService {
      * @param message
      */
     void create (Message message);
+
+
+    /**
+     * 根据用户id查询该用户未读数量
+     * @param receiveUserId
+     * @return
+     */
+    Integer selectUnreadCount( Long receiveUserId);
+
+
+    /**
+     * 根据接受者用户Id查询所有站内信
+     * @param receiveUserId
+     * @return
+     */
+    List<Message> selectByReceiveUserId (Long receiveUserId);
+
+    /**
+     * 更新指定站内信的状态
+     * @param id
+     * @param state
+     */
+    void updateStateById (Long id , Byte state);
+
+    /**
+     * 根据id查询站内信
+     * @param id
+     * @return
+     */
+    Message selectById (Long id);
+
+    /**
+     * 要回复对的站内信id
+     * @param repliedId
+     * @param message
+     */
+
+    void reply (Long repliedId, Message message);
+
+
+
 }
