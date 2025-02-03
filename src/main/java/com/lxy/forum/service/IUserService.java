5df9acbbc99a7bec2d4b2c9e5ec36b43489adca9
matexiang <matexy2024@126.com>
2025-02-03 15:39:02 +0800

    Update IUserService.java

--- src/main/java/com/lxy/forum/service/IUserService.java
+++ src/main/java/com/lxy/forum/service/IUserService.java
@@ -33,5 +33,16 @@ public interface IUserService {
      */
     void modifyInfo(User user);
 
+    /**
+     * 修改密码
+     * @param id
+     * @param newPassword
+     * @param oldPassword
+     */
+    void modifyPassword(Long id,String newPassword, String oldPassword);
+
 
 }
+
+
+
