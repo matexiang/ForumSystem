a3a29dfe5f47bb2ed730fd6c2ec16b376a8f22a0
matexiang <matexy2024@126.com>
2025-02-03 15:28:48 +0800

    Update MessageMapper.java

--- src/main/java/com/lxy/forum/dao/MessageMapper.java
+++ src/main/java/com/lxy/forum/dao/MessageMapper.java
@@ -2,6 +2,9 @@ package com.lxy.forum.dao;
 
 import com.lxy.forum.model.Message;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface MessageMapper {
@@ -14,4 +17,19 @@ public interface MessageMapper {
     int updateByPrimaryKeySelective(Message row);
 
     int updateByPrimaryKey(Message row);
+
+    /**
+     * 根据用户id查询该用户未读数量
+     * @param receiveUserId
+     * @return
+     */
+    Integer selectUnreadCount( @Param("receiveUserId") Long receiveUserId);
+
+    /**
+     * 根据接受者用户Id查询所有站内信
+     * @param receiveUserId
+     * @return
+     */
+    List<Message> selectByReceiveUserId ( @Param("receiveUserId") Long receiveUserId);
+
 }
